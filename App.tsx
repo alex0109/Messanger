9db5397c294fbc78a65c7c6d1a93e0cdb68df6c6
Dimasta229 <dmitrokytsenko@gmail.com>
2023-09-14 23:10:21 +0300

    Implementing of authorization

--- App.tsx
+++ App.tsx
@@ -1,26 +1,56 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 
-import ScreenOne from "./app/pages/Options/Options";
-import ScreenTwo from "./app/pages/Auth/Auth";
 import { ThemeProvider } from "./app/shared/lib/providers/ThemeProvider";
 
+import tokenCache from "./app/pages/Auth/SignInWithOAuth";
+import SignUpScreen from "./app/pages/Auth/SignUpScreen";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import DrawerNavigator from "./app/shared/lib/navigation/DrawerNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
 import { store } from "./app/shared/lib/store/store";
+import Constants from "expo-constants";
 
-const Drawer = createDrawerNavigator();
+import SignInScreen from "./app/pages/Auth/SignInScreen";
 
 export default function App() {
   return (
     <Provider store={store}>
       <ThemeProvider>
-        <NavigationContainer>
-          <DrawerNavigator />
-        </NavigationContainer>
+        {/*  */}
+
+        <ClerkProvider
+          publishableKey={Constants.expoConfig?.extra!.clerkPublishableKey}
+          tokenCache={tokenCache}
+        >
+          <SignedIn>
+            <NavigationContainer>
+              <DrawerNavigator />
+            </NavigationContainer>
+          </SignedIn>
+          <SignedOut>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <SignInScreen />
+            </View>
+          </SignedOut>
+        </ClerkProvider>
       </ThemeProvider>
     </Provider>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
