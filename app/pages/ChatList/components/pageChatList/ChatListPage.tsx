c0d8433931375b05a16b17475cbef4d8867c5a8c
Liroy248 <art27517@gmail.com>
2023-09-22 15:19:35 +0300

    added ChatList

--- app/pages/ChatList/components/pageChatList/ChatListPage.tsx
+++ app/pages/ChatList/components/pageChatList/ChatListPage.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import { Image } from "expo-image";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-
 import ChatListBase from "../ChatListBase/ChatListBase";
 import ChatListHeader from "../ChatListHeader/ChatListHeader";
 
 const ChatListPage = () => {
   return (
     <View>
+      
       <ChatListHeader />
       <View style={{ flex: 1 }}>
         <ChatListBase />
