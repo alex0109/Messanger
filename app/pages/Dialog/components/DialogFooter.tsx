9362d0d788de1d48e58fe31eaaa838b4faea20f9
Liroy248 <art27517@gmail.com>
2023-10-10 16:42:04 +0300

    DON

--- app/pages/Dialog/components/DialogFooter.tsx
+++ app/pages/Dialog/components/DialogFooter.tsx
@@ -1,17 +1,34 @@
-import { Ionicons } from "@expo/vector-icons";
-import React, { useState, useEffect } from "react";
-import { StyleSheet, View, TouchableHighlight, TextInput } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  TouchableHighlight,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+} from 'react-native';
 
-import { useNavigation, useTheme } from "@react-navigation/native";
+import { useNavigation, useTheme } from '@react-navigation/native';
+import EmojiSelector from 'react-native-emoji-selector';
 
-import type { FC } from "react";
-import colors from "@/shared/assets/styles/colors";
+import type { FC } from 'react';
+import colors from '@/shared/assets/styles/colors';
 
 //Входящие пропсы надо типизировать
 const Footer: FC = ({ setData, data }) => {
   //Отделяй константы, функции и методы энтерами
   //Константы групируй и отделяй между собой тоже, стейты отдельно, другие хуки отдельно(сортируй по библиотекам, например)
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
+  const [showEmojiSelector, setShowEmojiSelector] = useState(false);
+  const handleEmojiPress = () => {
+    setShowEmojiSelector(!showEmojiSelector);
+    Keyboard.dismiss();
+  };
+
+  const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => {
+    showEmojiSelector ? setShowEmojiSelector(!showEmojiSelector) : null;
+  });
 
   const colors = useTheme().colors;
 
@@ -19,37 +36,45 @@ const Footer: FC = ({ setData, data }) => {
 
   const sendMessageHandler = () => {
     if (input.length > 0) {
-      setData([
-        { id: `${Math.random(1, 1000)}`, title: input, index: 1 },
-        ...data,
-      ]);
-      setInput("");
+      setData([{ id: `${Math.random(1, 1000)}`, title: input, index: 1 }, ...data]);
+      setInput('');
     }
   };
 
   return (
-    <View style={[style.sendBar, { backgroundColor: colors.footer }]}>
-      <TextInput
-        multiline
-        placeholder="Type..."
-        placeholderTextColor={colors.adaptiveGrey}
-        style={[style.msgInput, { color: colors.adaptiveText }]}
-        cursorColor={colors.adaptiveText}
-        defaultValue={input}
-        onChangeText={(userInput) => setInput(userInput)}
-      />
-      <View style={[style.separator, { backgroundColor: colors.adaptiveGrey }]} />
-      <TouchableHighlight
-        style={style.sendButton}
-        onPress={() => sendMessageHandler()}
-      >
-        <Ionicons
-          name="send"
-          size={24}
-          color={colors.blue}
-          style={style.buttonIcon}
+    <View>
+      <View style={style.sendBar}>
+        <View style={[style.textInputBar, { backgroundColor: colors.footer }]}>
+          <TouchableOpacity
+            style={{ justifyContent: 'flex-end', alignItems: 'center', margin: 5 }}
+            onPress={() => handleEmojiPress()}>
+            <Ionicons name='happy-outline' size={30} color={colors.adaptiveText} />
+          </TouchableOpacity>
+          <TextInput
+            multiline
+            placeholder='Type...'
+            placeholderTextColor={colors.adaptiveGrey}
+            style={[style.msgInput, { color: colors.adaptiveText }]}
+            cursorColor={colors.adaptiveText}
+            defaultValue={input}
+            onChangeText={(userInput) => setInput(userInput)}
+          />
+        </View>
+        <TouchableHighlight
+          style={style.sendButton}
+          underlayColor={colors.mainBackground}
+          onPress={() => sendMessageHandler()}>
+          <Ionicons name='send' size={26} color={colors.blue} style={style.buttonIcon} />
+        </TouchableHighlight>
+      </View>
+      {showEmojiSelector && (
+        <EmojiSelector
+          style={{ height: 250 }}
+          onEmojiSelected={(emoji) => {
+            setInput((prevInput) => prevInput + emoji);
+          }}
         />
-      </TouchableHighlight>
+      )}
     </View>
   );
 };
@@ -57,30 +82,34 @@ const Footer: FC = ({ setData, data }) => {
 export default Footer;
 
 const style = StyleSheet.create({
-  separator: {
-    backgroundColor: "gray",
-    height: "70%",
-    alignSelf: "center",
-    width: 2,
-  },
   sendButton: {
-    height: 50,
-    width: 50,
-    justifyContent: "center",
+    justifyContent: 'center',
     borderRadius: 10,
+    padding: 5,
   },
   buttonIcon: {
-    alignSelf: "center",
+    alignSelf: 'center',
   },
   sendBar: {
-    flexDirection: "row",
-    backgroundColor: "white",
-    alignItems: "center",
+    marginHorizontal: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
   },
   msgInput: {
-    flex: 7,
-    margin: 12,
+    flex: 1,
+    marginVertical: 8,
     maxHeight: 100,
     fontSize: 16,
+    paddingRight: 8,
+  },
+  textInputBar: {
+    borderRadius: 25,
+    marginVertical: 5,
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    alignContent: 'center',
+    justifyContent: 'space-evenly',
+    flex: 1,
   },
 });
--- app/pages/Dialog/components/DialogHeader.tsx
+++ app/pages/Dialog/components/DialogHeader.tsx
@@ -1,9 +1,9 @@
-import { AntDesign, Entypo } from "@expo/vector-icons";
-import { useNavigation, useTheme } from "@react-navigation/native";
-import React from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { AntDesign, Entypo } from '@expo/vector-icons';
+import { useNavigation, useTheme } from '@react-navigation/native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
-import type { FC } from "react";
+import type { FC } from 'react';
 
 const DialogHeader: FC = () => {
   //Для навигации в приложении
@@ -15,26 +15,23 @@ const DialogHeader: FC = () => {
     <View style={[styles.header, { backgroundColor: colors.header }]}>
       <TouchableOpacity
         onPress={() => navigation.goBack()}
-        style={[styles.fullAlign, { marginHorizontal: 15 }]}
-      >
-        <AntDesign name="arrowleft" size={24} color={colors.white} />
+        style={[styles.fullAlign, { marginHorizontal: 15 }]}>
+        <AntDesign name='arrowleft' size={24} color={colors.white} />
       </TouchableOpacity>
       <View style={[{ flex: 1, borderRadius: 90 }, styles.fullAlign]}>
         <Image
           style={styles.userAvatar}
           source={{
-            uri: "https://cdn-icons-png.flaticon.com/512/266/266033.png",
+            uri: 'https://cdn-icons-png.flaticon.com/512/266/266033.png',
           }}
         />
       </View>
       <View style={[{ flex: 4 }, styles.topStatus]}>
         <Text style={[styles.username, { color: colors.white }]}>Username</Text>
-        <Text style={[styles.timeStatus, { color: colors.adaptiveGrey }]}>
-          last time 09:32
-        </Text>
+        <Text style={[styles.timeStatus, { color: colors.lightGrey }]}>last time 09:32</Text>
       </View>
       <TouchableOpacity style={[{ flex: 1 }, styles.fullAlign]}>
-        <Entypo name="dots-three-vertical" size={24} color={colors.white} />
+        <Entypo name='dots-three-vertical' size={24} color={colors.white} />
       </TouchableOpacity>
     </View>
   );
@@ -44,34 +41,34 @@ export default DialogHeader;
 
 const styles = StyleSheet.create({
   header: {
-    flexDirection: "row",
+    flexDirection: 'row',
     height: 50,
-    width: "100%",
-    alignItems: "center",
+    width: '100%',
+    alignItems: 'center',
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
   },
   userAvatar: {
-    height: "80%",
+    height: '80%',
     aspectRatio: 1,
     borderRadius: 30,
   },
   topStatus: {
-    height: "100%",
-    justifyContent: "center",
+    height: '100%',
+    justifyContent: 'center',
   },
   username: {
     fontSize: 12,
-    color: "#FFFFFF",
-    fontWeight: "600",
+    color: '#FFFFFF',
+    fontWeight: '600',
   },
   timeStatus: {
     fontSize: 12,
-    color: "#C0C0C0",
-    fontWeight: "400",
+    color: '#C0C0C0',
+    fontWeight: '400',
   },
   fullAlign: {
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
